64a988657cec922b5a7555875eb7a5f1269beac3
Dons <67947107+zadrot228lol@users.noreply.github.com>
2021-11-14 19:03:18 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -6,6 +6,31 @@ const bot = new telegramBotAPI(token, {
   polling: true,
 });
 
+const check = async (nickname, chatId) => {
+  if (/[а-яёА-ЯЁ]+/.test(nickname)) {
+    return bot.sendMessage(
+      chatId,
+      "Запрещено указывать русские символы в никах!"
+    );
+  }
+  const fraction = await getPlayerFraction(nickname);
+  if (!fraction.inited) {
+    return bot.sendMessage(
+      chatId,
+      `Происходит инициализация данных. Ожидайте. Это может длиться до двух минут. Если будет больше, то обратитесь к разработчикам.`
+    );
+  }
+
+  if (!fraction.isPlayerInFraction) {
+    return bot.sendMessage(chatId, "Игрок не состоит в организации");
+  }
+
+  return bot.sendMessage(
+    chatId,
+    `Название организации: ${fraction.orgName}\nДолжность(ранг): ${fraction.playerRank}\nДолжность(название): ${fraction.playerRankName}`
+  );
+};
+
 bot.onText(/\/org (.+)/, async (msg) => {
   const chatId = msg.chat.id;
   const { text } = msg;
@@ -16,27 +41,13 @@ bot.onText(/\/org (.+)/, async (msg) => {
       "Вы забыли указать никнейм. /org [nickname]"
     );
   }
-  if(/[а-яёА-ЯЁ]+/.test(splitedText[1])){
-    return bot.sendMessage(chatId, "Запрещено указывать русские символы в никах!")
+  if (splitedText[1] && splitedText[2]) {
+    if (
+      splitedText[1].split("_").length === 1 &&
+      splitedText[2].split("_").length === 1
+    ) {
+      return check(`${splitedText[1]}_${splitedText[2]}`, chatId);
+    }
   }
-  const fraction = await getPlayerFraction(splitedText[1]);
-  if(!fraction.inited){
-    return bot.sendMessage(
-      chatId,
-      `Происходит инициализация данных. Ожидайте. Это может длиться до двух минут. Если будет больше, то обратитесь к разработчикам.`
-    );
-  }
-
-  if(!fraction.isPlayerInFraction){
-    return bot.sendMessage(
-      chatId,
-      "Игрок не состоит в организации"
-    );
-  }
-
-  return bot.sendMessage(
-    chatId,
-    `Название организации: ${fraction.orgName}\nДолжность(ранг): ${fraction.playerRank}\nДолжность(название): ${fraction.playerRankName}`
-  );
-  
+  return check(splitedText[1], chatId);
 });
